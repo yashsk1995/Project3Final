fac7d27a724c54fd9ac3cfca9ceee41c4d575fa9
yashK <yashkinariwalask95@gmail.com>
2018-10-26 10:10:36 -0400

    done!

--- models/User.js
+++ models/User.js
@@ -3,47 +3,49 @@ const Schema = mongoose.Schema;
 
 // Create Schema
 const UserSchema = new Schema({
- name: {
-   type: String,
-   required: true
- },
- email: {
-   type: String,
-   required: true
- },
- avatar: {
-   type: String
- },
- password: {
-   type: String,
-   required: true
- },
- aboutMe: {
-   type: [String],
-   required: true
- },
- interestedIn: {
-   type: [String],
-   required: true
- },
- date: {
-   type: Date,
-   default: Date.now
- },
- saved:{
-   type:[String]
- },
- myAge: {
-   type:Number
- },
- myGender: {
-   type:String
- },
- myLocation: {
-   type:String
- }
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  myAge: {
+    type: Number,
+    required: true
+  },
+  myGender: {
+    type: String,
+    required: true
+  },
+  myLocation: {
+    type: String,
+    required: true
+  },
+  aboutMe: {
+    type: [String],
+    required: false
+  },
+  interestedIn: {
+    type: [String],
+    required: false
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  saved: {
+    type: [String],
+    required: false
+  }
 });
 
 module.exports = User = mongoose.model("users", UserSchema);
-
-
